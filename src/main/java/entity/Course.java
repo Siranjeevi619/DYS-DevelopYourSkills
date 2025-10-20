8ecfb8a70dafa4b36e37dc3e5312d2f55142017c
Siranjeevi619 <siranjeevi0619@gmail.com>
2025-10-20 08:08:09 +0530

    feat: added course enitity

---
+++ src/main/java/entity/Course.java
@@ -0,0 +1,43 @@
+package entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection="course")
+public class Course {
+
+
+    @Id
+    private String id;
+    private String title;
+    private String description;
+    private String author;
+    private String category;
+    private String authorLogo;
+    private String thumbnailUrl;
+    private List<String> videoUrls;
+    private List<String> videoTitle;
+    private List<String> documents;
+    private List<String> certifications;
+    private List<String> tags;
+
+
+    @CreatedDate
+    private LocalDateTime createAt;
+
+    @LastModifiedDate
+    private LocalDateTime updateAt;
+}
